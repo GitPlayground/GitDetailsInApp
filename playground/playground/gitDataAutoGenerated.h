e52483ab25ae3ab20e2347681545989872fe1818
mdaslamansari2008 <aslam.a@osmosys.asia>
2015-12-01 10:35:12 +0530

    Added two new files in the repo

---
+++ playground/playground/gitDataAutoGenerated.h
@@ -0,0 +1,12 @@
+//-----------------------------------------
+// Auto generated file
+// Created 2015-12-01 10:31:20
+//-----------------------------------------
+
+#define BUILD_DATE              @"2015-12-01 10:31:20"
+#define GIT_CURRENT_BRANCH      @"master"
+#define GIT_LAST_COMMIT_HASH    @"6949689"
+#define GIT_LAST_COMMIT_DATE    @"2015-12-01"
+#define GIT_LAST_COMMIT_COMMENT @"Adding new files"
+#define GIT_LAST_REPO_TAG       @""
+#define GIT_REPO_TAG            @"https://github.com/GitPlayground/osmplay.git"
