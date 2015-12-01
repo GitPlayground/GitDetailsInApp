6949689b554b08fc89e9d8e001ab730c3a13c9e6
mdaslamansari2008 <aslam.a@osmosys.asia>
2015-12-01 09:24:50 +0530

    Adding new files

---
+++ playground/playground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  playground
+//
+//  Created by Aslam Ansari on 01/12/15.
+//  Copyright © 2015 Osmosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ playground/playground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  playground
+//
+//  Created by Aslam Ansari on 01/12/15.
+//  Copyright © 2015 Osmosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
